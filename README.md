b7253ca99ba90cee0827546e4f34649b56aa609b
Susheel Thapa <susheel.thapa@outlook.com>
2022-11-29 15:31:17 +0545

    Fixed issue #1
    Updated README file with requirement section and to open the web broswer
    to see the web page
    
    Added comment in css to identify to which ejs the css is being applied
    
    Remove the unnecessary package and unnecessary import.
    
    Remove extra div from the footer

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
 ## About the project
+
 A blog project, built using the following technologies:
 
 **EJS, Node.js, Express.js, MongoDB**
+
+# Blog
+
+The project is made using
+
+- [ejs](https://ejs.co/)
+- [express](https://expressjs.com/)
+- [mongodb](https://www.mongodb.com/) & [mongoose](https://www.npmjs.com/package/mongoose)
+- [nodemon](https://www.npmjs.com/package/nodemon)
+- [nodejs](https://www.nodejs.org)
+
+
+## Requirement
+
+1. You must have **MongoDB** install and setup properly in your machine
+2. You must have install the latest version of **nodejs**
 
 ## Running the project
 
 To run the project, go to the project directory and use the commands:
 
-```bash
-  npm install
-```
-```bash
-  npm start
-```
+1. Installing the node modules
+
+   ```sh
+     npm install
+   ```
+
+2. Running the server
+
+   ```sh
+     npm start
+   ```
+
+3. **Open your web browser and search _localhost:5000_**
--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const path = require("path");
+
 const mongoose = require("mongoose");
 
 const app = express();
@@ -10,52 +8,52 @@ mongoose.connect("mongodb://localhost:27017/blogDB");
 
 const blogSchema = {
   title: String,
-  content: String
-}
+  content: String,
+};
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   Blog.find({}, function (err, blogs) {
     res.render("home", {
-      posts_: blogs
+      posts_: blogs,
     });
-  })
-})
+  });
+});
 
 app.get("/compose", function (req, res) {
   res.render("compose");
-})
+});
 
 app.post("/compose", function (req, res) {
   const post = {
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
   };
   const blog = new Blog({
     title: post.title,
-    content: post.content
-  })
+    content: post.content,
+  });
   blog.save(function (err) {
     if (!err) {
       res.redirect("/");
     }
   });
-})
+});
 
 app.get("/posts/:blogID", function (req, res) {
   const requestedBlogId = req.params.blogID;
   Blog.findOne({ _id: requestedBlogId }, function (err, blog) {
     res.render("post", {
       title: blog.title,
-      content: blog.content
-    })
-  })
-})
+      content: blog.content,
+    });
+  });
+});
 
 app.listen(5000);
